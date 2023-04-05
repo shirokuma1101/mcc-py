2d9c4757fed979feaf4d64d763a0171e64608713
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-04-06 03:27:56 +0900

    add: ソースファイルを追加

---
+++ mccpy/mccmanager.py
@@ -0,0 +1,34 @@
+# standard
+import subprocess
+import threading
+
+#
+from mccpy.mccutil import MccUtil
+
+
+class MccManager:
+
+    # public
+
+    def __init__(self) -> None:
+        self.mc_process = None
+        self.mc_thread = None
+
+    def start(self, start_path: str, encoding: str='utf-8') -> None:
+        self.mc_process = subprocess.Popen(start_path,
+                                           stdin=subprocess.PIPE,
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.STDOUT,
+                                           shell=True)
+        def log() -> None:
+            for line in MccUtil.getstdout(self.mc_process, encoding=encoding):
+                print(line)
+                #todo parse and write to log
+
+        self.mc_thread = threading.Thread(target=log)
+        self.mc_thread.start()
+
+    def stop(self) -> None:
+        MccUtil.inputstdin('stop', self.mc_process)
+        self.mc_thread.join()
+
---
+++ mccpy/mccutil.py
@@ -0,0 +1,60 @@
+# standard
+import os
+import subprocess
+from urllib.parse import urlparse
+
+# scraping
+import requests
+from bs4 import BeautifulSoup
+
+
+class MccUtil:
+
+    # public
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def download(header: str, download_dir: str, edition: str, platform: str = 'win', version: str = None) -> bool:
+        dir_url = MccUtil._getmcurl(header=header, edition=edition, platform=platform, version=version)
+        if dir_url:
+            res = requests.get(dir_url, headers={'User-Agent': header})
+            if res.ok:
+                with open(f'{download_dir}/{os.path.basename(urlparse(dir_url).path)}', 'wb') as f:
+                    f.write(res.content)
+                return True
+
+        return False
+
+    @staticmethod
+    def getstdout(proc: subprocess.Popen, encoding: str='utf-8') -> str:
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            yield line.decode(encoding).strip()
+
+    @staticmethod
+    def inputstdin(cmd: str, proc: subprocess.Popen, encodeing: str='utf-8', nc: str='\r\n') -> None:
+        proc.stdin.write(f'{cmd}{nc}'.encode(encodeing))
+        proc.stdin.flush()
+
+    # private
+    @staticmethod
+    def _getmcurl(header: str, edition: str, platform: str = 'win', version: str = None) -> str:
+        if edition == 'je':
+            pass
+        elif edition == 'be':
+            mc_url = 'https://www.minecraft.net/en-us/download/server/bedrock'
+            res = requests.get(mc_url, headers={'User-Agent': header})
+            if res.ok:
+                soup = BeautifulSoup(res.text, 'html.parser')
+                if platform == 'win':
+                    soup = soup.find('a', {'data-platform': f'serverBedrockWindows'})
+                elif platform == 'linux':
+                    soup = soup.find('a', {'data-platform': f'serverBedrockLinux'})
+                return soup.get('href')
+
+        return None
+
